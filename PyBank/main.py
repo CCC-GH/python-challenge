282d93f6120f48a3ebb0bb02f3f166324c237d83
CCC-GH <70607540+CCC-GH@users.noreply.github.com>
2020-10-03 23:29:01 -0500

    Final Commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,30 +2,42 @@
 """
 03-Python-HW
 python-challenge/PyBank 
+
+This script will scan rows for a count of months, total profit, average, greatest increase/decrease in pofits (not difference from previous) 
+
 """
-import csv
+# Initialize Counts
 netTotal = 0
 rowCount = 0
 maxProfit = 0
 minProfit = 0
+# Open CSV PyBank file
+import csv
 with open(r'C:\Users\coffm\CarlNU\python-challenge\PyBank\Resources\03-Python_HW_Instructions_PyBank_Resources_budget_data.csv') as csvfile:
     csvreader = csv.reader(csvfile)
+    # Skip Header Record
     next(csvreader)
+    # Scan rows in PyBank file
     for row in csvreader:
+    # Set Max and Min with first 
         if rowCount == 0:
             maxProfit = int(row[1])
             minProfit = int(row[1])
+        # Count number of months
         rowCount = rowCount + 1
+        # Sum total
         netTotal = netTotal + int(row[1])
+        # Check to see if this month is Max/Min Profit
         if int(row[1]) > maxProfit:
             maxProfit = int(row[1])
             maxDate = row[0]
         if int(row[1]) < minProfit:
             minProfit = int(row[1])
             minDate = row[0] 
+    # Write report to OutputPyBank.txt, text file
     outFile = open(r'C:\Users\coffm\CarlNU\python-challenge\PyBank\analysis\OutputPyBank.txt','w')
     outFile.write("\nFinancial Analysis \n")
-    outFile.write("------------------------------------- \n")
+    outFile.write("-"*50 + "\n")
     outFile.write("Total Months: " + str(rowCount) + " \n")   
     outFile.write("Total: $" + str(netTotal) + " \n") 
     outFile.write("Average Change: $" + str(round(netTotal/rowCount,2)) + " \n")
@@ -33,6 +45,7 @@ with open(r'C:\Users\coffm\CarlNU\python-challenge\PyBank\Resources\03-Python_HW
     outFile.write("Greatest Decrease in Profits: " + minDate + " ($" + str(minProfit) +") \n")
     outFile.close()
     outFile = open(r'C:\Users\coffm\CarlNU\python-challenge\PyBank\analysis\OutputPyBank.txt','r')
+    # Print report from output file, OutputPyBank.txt
     print(outFile.read())
  
     
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,27 +3,68 @@
 03-Python-HW
 python-challenge/PyPoll 
 """
-import csv
 rowCount = 0
+import csv
 with open(r'C:\Users\coffm\CarlNU\python-challenge\PyPoll\Resources\03-Python_HW_Instructions_PyPoll_Resources_election_data.csv') as csvfile:
     csvreader = csv.reader(csvfile)
     next(csvreader)
     for row in csvreader:
-        if rowCount == 0:
-            rowCount = rowCount + 1   
+        rowCount = rowCount + 1   
     outFile = open(r'C:\Users\coffm\CarlNU\python-challenge\PyPoll\analysis\OutputPyPoll.txt','w')
     outFile.write("\nElection Results \n")
-    outFile.write("---------------------------- \n")
+    outFile.write("-"*30 + "\n")
     outFile.write("Total Votes: " + str(rowCount) + " \n")  
-    outFile.write("---------------------------- \n")
+    outFile.write("-"*30 + "\n")
     outFile.write("Candidate 1 \n")  
     outFile.write("Candidate 2 \n")  
     outFile.write("Candidate 3 \n")  
     outFile.write("Candidate 4 \n") 
-    outFile.write("---------------------------- \n")
+    outFile.write("-"*30 + "\n")
     outFile.write("Winner: " + "winner " + "\n")
-    outFile.write("---------------------------- \n")    
+    outFile.write("-"*30 + "\n")   
     outFile.close()
     outFile = open(r'C:\Users\coffm\CarlNU\python-challenge\PyPoll\analysis\OutputPyPoll.txt','r')
     print(outFile.read())
    
+    
+candidateList = []   
+import csv
+with open(r'C:\Users\coffm\CarlNU\python-challenge\PyPoll\Resources\03-Python_HW_Instructions_PyPoll_Resources_election_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader)
+    for row in csvreader:
+        rowCount = rowCount + 1   
+        if row[2] not in candidateList:
+            candidateList.append(row[2])
+    for candidate in set(candidateList):
+        print(candidate)
+        print(rowCount)
+        print(candidateList.count(candidate))        
+        
+        
+        next(csvreader)
+        
+        for row in csvreader:
+            if rowCount == 0:
+                rowCount = rowCount + 1  
+            if row[2] = candidate[0]
+            
+            
+        votes = candidateList.count(candidate)
+        
+        
+        row in csvreader:  
+        
+        print(str(votes))
+        print(str(round(votes/rowCount*100,3)))
+        
+ 
+        if maxVoteCount > voteCount:
+            winner = candidate
+        print(int(row) + ": " +  str(round(voteCount/rowCount,3)) + "% (" + str(votecount))")")
+        voteCount = 0
+    Print(winner)
+    outfile.write(candidate)
+    
+        
+        
