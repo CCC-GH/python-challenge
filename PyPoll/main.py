399b56a52ec3cd2169bf8ee93d4d772f4d2953ba
CCC-GH <70607540+CCC-GH@users.noreply.github.com>
2020-10-05 23:59:16 -0500

    Test Commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,68 +1,52 @@
-# -*- coding: utf-8 -*-
 """
 03-Python-HW
 python-challenge/PyPoll 
+
+This Python script will scan rows for candidates total votes, candidates' votes and their %-of-total, and the candidate winner of the election.
+
+Write results to report file, OutputPyPoll.txt, and when finished, print report file to terminal.
+
 """
-rowCount = 0
-import csv
-with open(r'\Users\coffm\CarlNU\python-challenge\PyPoll\Resources\03-Python_HW_Instructions_PyPoll_Resources_election_data.csv') as csvfile:
-    csvreader = csv.reader(csvfile)
-    next(csvreader)
-    for row in csvreader:
-        rowCount = rowCount + 1   
-    outFile = open(r'C:\Users\coffm\CarlNU\python-challenge\PyPoll\analysis\OutputPyPoll.txt','w')
-    outFile.write("\nElection Results \n")
-    outFile.write("-"*30 + "\n")
-    outFile.write("Total Votes: " + str(rowCount) + " \n")  
-    outFile.write("-"*30 + "\n")
-    outFile.write("Candidate 1 \n")  
-    outFile.write("Candidate 2 \n")  
-    outFile.write("Candidate 3 \n")  
-    outFile.write("Candidate 4 \n") 
-    outFile.write("-"*30 + "\n")
-    outFile.write("Winner: " + "winner " + "\n")
-    outFile.write("-"*30 + "\n")   
-    outFile.close()
-    outFile = open(r'C:\Users\coffm\CarlNU\python-challenge\PyPoll\analysis\OutputPyPoll.txt','r')
-    print(outFile.read())
-   
-print([i+1 for i in range(100)])
-
-
-import csv
+# Initialize Counts
 candidateList = [] 
 totalVotes = 0
 voteCount = 0
-winner = "carl"
-with open(r'\Users\coffm\CarlNU\python-challenge\PyPoll\Resources\03-Python_HW_Instructions_PyPoll_Resources_election_data.csv') as csvfile:
+winnerCount = 0
+# Open CSV PyPoll 
+import csv
+with open(r'\Users\coffm\CarlNU\python-challenge\PyPoll\Resources\03-Python_HW_Instructions_PyPoll_Resources_election_data_election_data.csv') as csvfile:
     csvreader = csv.reader(csvfile)
+    # Skip Header Record
     next(csvreader)
+     # Scan rows in PyPoll csv file for distinct list of candidates and total votes
     for row in csvreader:      
         totalVotes = totalVotes + 1   
         if row[2] not in candidateList:
             candidateList.append(row[2])
+    # Open report file output file, outputPyPoll.txt, to write header and total votes (this file will be printed at end)
+    outFile = open(r'C:\Users\coffm\CarlNU\python-challenge\PyPoll\analysis\OutputPyPoll.txt','w')
+    outFile.write("\nElection Results \n")
+    outFile.write("-"*23 + "\n")
+    outFile.write("Total Votes: " + str(totalVotes) + " \n") 
+    outFile.write("-"*23 + "\n")
+    # Scan PyPoll csv file for each candidate counting votes and check for Winner
     for candidate in candidateList:
-        print(candidate)
-        for row2 in csvreader:
-            print(row2[2])
-            print(candidate)
-            if candidate == row2[2]:
+        csvfile.seek(0)
+        for row in csvreader:
+            if candidate == row[2]:
                 voteCount = voteCount + 1
-                if voteCount > winnerCount:
-                    winner = candidate
-        print(candidate + " " + str(voteCount))
-        voteCount = 0   
-    print(winner)        
-            
-             
-            
- 
-        if maxVoteCount > voteCount:
+        if voteCount > winnerCount:
             winner = candidate
-        print(int(row) + ": " +  str(round(voteCount/rowCount,3)) + "% (" + str(votecount))")")
-        voteCount = 0
-    Print(winner)
-    outfile.write(candidate)
-    
-        
-        
+            winnerCount = voteCount
+        # Write each candidate, their number of votes, and percentage of total votes
+        outFile.write(candidate + ": " + str(round(voteCount/totalVotes*100,4)) + "% (" + str(voteCount) + ")\n")
+        voteCount = 0   
+    # Write winner candidate with greatest number of votes
+    outFile.write("-"*23 + "\n")
+    outFile.write("Winner: " + winner + "\n")
+    outFile.write("-"*23 + "\n")   
+    outFile.close()
+    # Print report from output file, OutputPyBank.txt
+    outFile = open(r'\Users\coffm\CarlNU\python-challenge\PyPoll\analysis\OutputPyPoll.txt','r')
+    print(outFile.read())
+     
